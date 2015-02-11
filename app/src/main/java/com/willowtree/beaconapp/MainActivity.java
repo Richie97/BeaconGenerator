9f80db33e280dbef12af63ad20b412a893f72d74
Eric Richardson <eric.richardson97@gmail.com>
2015-02-11 17:11:30 -0500

    First Commit

---
+++ app/src/main/java/com/willowtree/beaconapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.willowtree.beaconapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends Activity {
+    BeaconTransmitter beaconShooter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Beacon beacon = new Beacon.Builder()
+                .setId1("02FC4494-B0D3-43D6-869A-E9841DA77E39")
+                .setId2("1071")
+                .setId3("2")
+                .setManufacturer(0x0118)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[]{0l}))
+                .build();
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+        beaconShooter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconShooter.startAdvertising(beacon);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        beaconShooter.stopAdvertising();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
